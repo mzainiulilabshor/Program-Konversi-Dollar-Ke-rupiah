cdfe45a0b54fdfca3cd3b79dac944d515a830f6c
mzainiulilabshor <45529539+mzainiulilabshor@users.noreply.github.com>
2019-01-05 02:11:05 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main (){
+	int Dollar;
+    cout<<"Masukkan nilai Dollar = $ ";
+    cin>>Dollar;
+    float Rupiah;
+    Rupiah= Dollar*15000;
+    cout<<"Hasil pertukaran = Rp. "<<Rupiah<< " ,00"<<endl;
+    getch();
+}
+
